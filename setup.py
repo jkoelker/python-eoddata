6f574fd76422cc2e87bcbc423932be9341fc097e
Jason Kölker <jason@koelker.net>
2013-08-23 19:35:44 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '0.1'
+
+setup(name='eoddata',
+      version=version,
+      description="Python wrapper around EOD Data's Webservice API",
+      long_description=open('README.rst').read(),
+      keywords='',
+      author='Jason Kölker',
+      author_email='jason@koelker.net',
+      url='https://github.com/jkoelker/python-eoddata',
+      license='MIT',
+      packages=['eoddata'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'suds',
+      ],
+      )
